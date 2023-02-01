fc3b1288cd5534e1ea1d6f6ee4713fc9f86de5d5
peter.sercombe <peter.sercombe@bcc.net.au>
2023-02-02 08:53:38 +1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+# This is a program that will allow me to order pizza
+# Copyright Peter Sercombe 2022
+# CC-BY-SA-4.0
+
+print("Welcome to Pete's a Pizza!")
+
+# size
+size = input("What size pizza would you like? ")
+# topping
+topping = input("What kind of topping would you like? ")
+# sauce
+sauce = input("What sauce would you like? ")
+# base
+base = input("What base would you like? ")
+# extra toppings
+extraToppings = input("What extra toppings would you like? ")
+
+
+# sides
+
+# drinks
+
+
+
+
