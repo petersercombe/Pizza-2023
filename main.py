67c0de3330c8302f1c3b9c50baf0f83575485510
Learner.Link Admin <help@learner.link>
2023-02-16 10:07:29 +1000

    Feb 13th

--- main.py
+++ main.py
@@ -13,36 +13,56 @@ def userInput(options):
             selection = int(input("Input your choice: "))
             # Check that the number is a menu option.
             if 0 <= selection < len(options):
-                return selection
+                return list(options)[selection]
             print("Please enter a valid choice.")
         except ValueError:
             print("Please enter a number")
 
 
 print("Welcome to Pete's a Pizza!")
-# size
-sizeOptions = {"New Yorker":5.00,
-               "Large":3.00,
-               "Small":0.00}
-listMenu(sizeOptions)
-size = userInput(sizeOptions)
-# topping
-toppingOptions = {"Meatlovers":0.0,
-                  "Pepperoni":0.0,
-                  "Vegan":3.0,
-                  "Vegorama":1.0,
-                  "Avocado":4.0,
-                  "Margherita":0.0,
-                  "Cheese":0.0,
-                  "Gourmet Fish Breath":15.0}
-listMenu(toppingOptions)
-topping = input("What kind of topping would you like? ")
-# sauce
-sauce = input("What sauce would you like? ")
-# base
-base = input("What base would you like? ")
-# extra toppings
-extraToppings = input("What extra toppings would you like? ")
+
+pizzas = []
+price = 0.0
+
+while True:
+    # size
+    sizeOptions = {"New Yorker":9.00,
+                   "Large":6.00,
+                   "Small":4.00}
+    listMenu(sizeOptions)
+    size = userInput(sizeOptions)
+    # topping
+    toppingOptions = {"Meatlovers":0.0,
+                      "Pepperoni":0.0,
+                      "Vegan":3.0,
+                      "Vegorama":1.0,
+                      "Avocado":4.0,
+                      "Margherita":0.0,
+                      "Cheese":0.0,
+                      "Gourmet Fish Breath":15.0}
+    listMenu(toppingOptions)
+    topping = userInput(toppingOptions)
+    # sauce
+    sauceOptions = {"BBQ":0.0,
+                    "Tomato":0.0,
+                    "Bechemal":1.0,
+                    "None":-1.0}
+    listMenu(sauceOptions)
+    sauce = userInput(sauceOptions)
+    # base
+    baseOptions = {"Classic Crust":0.0,
+                   "Deep Pan": 0.0,
+                   "Thin 'n' Crispy":0.0,
+                   "Gluten Free": 2.0}
+    listMenu(baseOptions)
+    base = userInput(baseOptions)
+
+    pizzas.append(f"{size} {topping} with {sauce} sauce on {base} base")
+
+    if input("Would you like more pizza? y/n: ").lower() == "y":
+        continue
+    else:
+        break
 
 
 # sides
@@ -50,5 +70,7 @@ extraToppings = input("What extra toppings would you like? ")
 # drinks
 
 
-
+print("Your order is: ")
+for pizza in pizzas:
+    print(pizza)
 
