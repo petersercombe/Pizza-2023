f071d7d9595285750dd11060ac50e1a3b8fdfaba
Learner.Link Admin <help@learner.link>
2023-02-16 11:29:27 +1000

    Feb 16th

--- main.py
+++ main.py
@@ -2,17 +2,23 @@
 # Copyright Peter Sercombe 2022
 # CC-BY-SA-4.0
 
+from menu import *
+
 def listMenu(options):
     for i, (key, value) in enumerate(options.items()):
         print("{} - {} (+${:.2f})".format(i, key, value))
 
 def userInput(options):
+    global price
+    # List the menu items:
+    listMenu(options)
     # Check the input is a number - integer
     while True:
         try:
             selection = int(input("Input your choice: "))
             # Check that the number is a menu option.
             if 0 <= selection < len(options):
+                price += list(options.values())[selection]
                 return list(options)[selection]
             print("Please enter a valid choice.")
         except ValueError:
@@ -26,46 +32,22 @@ price = 0.0
 
 while True:
     # size
-    sizeOptions = {"New Yorker":9.00,
-                   "Large":6.00,
-                   "Small":4.00}
-    listMenu(sizeOptions)
     size = userInput(sizeOptions)
     # topping
-    toppingOptions = {"Meatlovers":0.0,
-                      "Pepperoni":0.0,
-                      "Vegan":3.0,
-                      "Vegorama":1.0,
-                      "Avocado":4.0,
-                      "Margherita":0.0,
-                      "Cheese":0.0,
-                      "Gourmet Fish Breath":15.0}
-    listMenu(toppingOptions)
     topping = userInput(toppingOptions)
     # sauce
-    sauceOptions = {"BBQ":0.0,
-                    "Tomato":0.0,
-                    "Bechemal":1.0,
-                    "None":-1.0}
-    listMenu(sauceOptions)
     sauce = userInput(sauceOptions)
     # base
-    baseOptions = {"Classic Crust":0.0,
-                   "Deep Pan": 0.0,
-                   "Thin 'n' Crispy":0.0,
-                   "Gluten Free": 2.0}
-    listMenu(baseOptions)
     base = userInput(baseOptions)
 
     pizzas.append(f"{size} {topping} with {sauce} sauce on {base} base")
-
     if input("Would you like more pizza? y/n: ").lower() == "y":
         continue
     else:
         break
 
-
 # sides
+
 
 # drinks
 
@@ -73,4 +55,4 @@ while True:
 print("Your order is: ")
 for pizza in pizzas:
     print(pizza)
-
+print("For a total of ${:.2f}".format(price) )
---
+++ menu.py
@@ -0,0 +1,34 @@
+sizeOptions = {"New Yorker": 9.00,
+               "Large": 6.00,
+               "Small": 4.00}
+
+toppingOptions = {"Meatlovers":0.0,
+                  "Pepperoni":0.0,
+                  "Vegan":3.0,
+                  "Vegorama":1.0,
+                  "Avocado":4.0,
+                  "Margherita":0.0,
+                  "Cheese":0.0,
+                  "Gourmet Fish Breath":15.0}
+
+sauceOptions = {"BBQ": 0.0,
+                "Tomato": 0.0,
+                "Bechemal": 1.0,
+                "None": -1.0}
+
+baseOptions = {"Classic Crust":0.0,
+               "Deep Pan": 0.0,
+               "Thin 'n' Crispy":0.0,
+               "Gluten Free": 2.0}
+
+sideOptions = {"Garlic bread": 3.0,
+               "Vegan Cheesy Garlic Bread": 5.0,
+               "Meatballs": 2.0,
+               "Spud fireballs": 3.0}
+
+drinkOptions = {"Coca-Cola": 2.0,
+                "Fanta": 2.0,
+                "Sprite": 2.0,
+                "Diet Coke": 2.0,
+                "Bundaberg Brewed Ginger Beer": 3.0,
+                "Mount Franklin Still Water": 4.0}
