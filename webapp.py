89ba6ec0cefff730db64ff732df583310ee1b465
Learner.Link Admin <help@learner.link>
2023-03-07 10:59:27 +1000

    March 7th

--- webapp.py
+++ webapp.py
@@ -1,14 +1,20 @@
 from flask import *
 
 app = Flask("__main__")
-app.secret_key = "laskjdf;lkajsdflkaj;sdflkjas"
+app.secret_key = "LKjskejdv9w9er8"
+
+@app.route("/")
+@app.route("/home")
+@app.route("/index")
+def home():
+    return render_template("index.html")
+
+@app.route("/profile/<username>")
+def profile(username):
+    return f"Hello, {username}!"
 
 
 
-
-
-
-
-
-app.run(Debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
 
