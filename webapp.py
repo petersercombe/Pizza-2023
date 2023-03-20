f5a8cb0503f3c06e78311e327a9a2aa0c09c2ec5
Learner.Link Admin <help@learner.link>
2023-03-20 14:59:54 +1000

    March 20th

--- webapp.py
+++ webapp.py
@@ -12,11 +12,20 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/menu")
+@app.route("/menu", methods=['GET'])
 def menu():
-    return render_template("menu.html", toppingOptions=toppingOptions)
+    collection = request.args['collection']
+    return render_template("menu.html", toppingOptions=toppingOptions, collection=collection)
 
+@app.route("/customise", methods=['GET', "POST"])
+def customise():
+
+    return render_template("customise.html",
+                           sizeOptions=sizeOptions,
+                           sauceOptions=sauceOptions,
+                           baseOptions=baseOptions
+                           )
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
 
