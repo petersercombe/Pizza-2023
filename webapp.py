c25d8e6211a267b40b90a391a14ef8471f25af2f
Learner.Link Admin <help@learner.link>
2023-04-21 09:46:22 +1000

    April 21th

--- webapp.py
+++ webapp.py
@@ -17,6 +17,7 @@ def menu():
     collection = request.args['collection']
     return render_template("menu.html", toppingOptions=toppingOptions, collection=collection)
 
+
 @app.route("/customise", methods=['GET', "POST"])
 def customise():
     pizza = request.args["pizza"]
@@ -24,9 +25,31 @@ def customise():
                            sizeOptions=sizeOptions,
                            sauceOptions=sauceOptions,
                            baseOptions=baseOptions,
-                           pizzaDetails=toppingOptions[pizza]
+                           pizzaDetails=toppingOptions[pizza],
+                           pizza=pizza
                            )
 
+
+@app.route("/cart", methods=["GET", "POST"])
+def cart():
+    if request.method == "GET":
+        return render_template("cart.html", order=session['order'])
+    else:
+        if 'order' not in session:
+            session['order'] = []
+        price = toppingOptions[request.form['pizza']]['price'] + \
+                sizeOptions[request.form['size']] + \
+                baseOptions[request.form['base']] + \
+                sauceOptions[request.form['sauce']]
+        session['order'].append([request.form['pizza'],
+                      request.form['size'],
+                      request.form['base'],
+                      request.form['sauce'],
+                      price])
+        session.modified = True
+        return redirect(url_for('cart'))
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
 
